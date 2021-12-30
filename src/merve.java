14a830b18a46523305a7f6717354e6c0a5735e64
MerveAcr <96839001+MerveAcr@users.noreply.github.com>
2021-12-30 20:26:44 +0300

    Update merve.java

--- src/merve.java
+++ src/merve.java
@@ -4,6 +4,6 @@ public class merve {
     }
     public static void main(String[] args) {
         System.out.println(sum(5,10));
-        System.out.println("Hello world!);
+        System.out.println("Hello world!");
     }
 }
